055567c0985b4718b2244befb489d56e032b1769
Nikolay <foitelija777@gmail.comm>
2022-05-04 16:39:39 +0300

    feat: add {post} and {get/id}

--- Controllers/SuperHeroController.cs
+++ Controllers/SuperHeroController.cs
@@ -8,15 +8,50 @@ namespace SuperHeroAPI.Controllers
     [ApiController]
     public class SuperHeroController : ControllerBase
     {
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero 
+                {
+                    Id = 1,
+                    Name ="Spider Man",
+                    FirstName ="Peter",
+                    LastName = "Parket",
+                    BornPlace = "New York"
+                },
+                 new SuperHero
+                {
+                    Id = 2,
+                    Name ="Iron Man",
+                    FirstName ="Tony",
+                    LastName = "Stark",
+                    BornPlace = "Long Island"
+                }
+            };
+
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
-            var heroes = new List<SuperHero>
-            {
-                new SuperHero {Id = 1, Name ="Spider Man", FirstName ="Peter", LastName = "Parket", BornPlace = "New York"}
-            };
             return Ok(heroes);
         }
+        [HttpGet("{id}")]
+        
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found.");
+            return Ok(hero);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+      
         
     }
 }
